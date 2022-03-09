1fdd9cd350735fed69c3d11b05606595a0714f5c
NePalDevelop <96785307+NePalDevelop@users.noreply.github.com>
2022-03-09 07:58:21 +0300

    Validation

--- WebApiGames/Models/Game.cs
+++ WebApiGames/Models/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApiGames.Models
 {
@@ -6,8 +7,11 @@ namespace WebApiGames.Models
     {
         public int Id { get; set; }
 
+        [StringLength(99, MinimumLength =3)]
         public string Name { get; set; }
 
+        [Required]
+        [Range(0, 999)]
         public int StudioId { get; set; }
 
         public string DeveloperStudioName { get; set; }
